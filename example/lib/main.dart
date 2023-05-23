2429dbbac54d2965006e923af19317edc477e712
Paulo Weslley <vorfast@gmail.com>
2023-05-22 21:12:51 -0300

    ajustes core

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -41,12 +41,14 @@ class _MyHomePageState extends State<MyHomePage> {
   void _checkConnection() async {
     _value = await checarConeccaoUsecase(
       parameters: NoParams(
-        error: ErrorReturnResult(
-          message: "Erro de conexão",
+        basic: ParametersBasic(
+          error: ErrorReturnResult(
+            message: "Erro de conexão",
+          ),
+          nameFeature: "Checar Conecção",
+          showRuntimeMilliseconds: true,
+          isIsolate: true,
         ),
-        nameFeature: "Checar Conecção",
-        showRuntimeMilliseconds: true,
-        isIsolate: true,
       ),
     );
 
@@ -113,11 +115,11 @@ class ConnectivityDatasource implements Datasource<bool> {
     try {
       final result = await isOnline;
       if (!result) {
-        throw parameters.error..message = "Você está offline";
+        throw parameters.basic.error..message = "Você está offline";
       }
       return result;
     } catch (e) {
-      throw parameters.error..message = "$e";
+      throw parameters.basic.error..message = "$e";
     }
   }
 }
