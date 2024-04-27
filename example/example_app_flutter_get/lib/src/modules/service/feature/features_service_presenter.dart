cb4a16d5d9a211b9613270bd78f02fa9fcf769a3
pwlimaverde <pwlimaverde@gmail.com>
2024-04-26 22:52:08 -0300

    presenter transformado em singleton

--- example/example_app_flutter_get/lib/src/modules/service/feature/features_service_presenter.dart
+++ example/example_app_flutter_get/lib/src/modules/service/feature/features_service_presenter.dart
@@ -3,18 +3,29 @@ import 'package:get/get.dart';
 import 'package:return_success_or_error/return_success_or_error.dart';
 
 final class FeaturesServicePresenter {
+  static FeaturesServicePresenter? _instance;
+
   final UsecaseBaseCallData<Unit, WidgetsBinding> _widgetsFlutterBindingUsecase;
 
-  FeaturesServicePresenter({
+  FeaturesServicePresenter._({
     required UsecaseBaseCallData<Unit, WidgetsBinding>
         widgetsFlutterBindingUsecase,
   }) : _widgetsFlutterBindingUsecase = widgetsFlutterBindingUsecase;
+
+  factory FeaturesServicePresenter({
+    required UsecaseBaseCallData<Unit, WidgetsBinding>
+        widgetsFlutterBindingUsecase,
+  }) {
+    _instance ??= FeaturesServicePresenter._(
+      widgetsFlutterBindingUsecase: widgetsFlutterBindingUsecase,
+    );
+    return _instance!;
+  }
 
   Future<Unit?> widgetsFlutterBinding(NoParams params) async {
     final data = await _widgetsFlutterBindingUsecase(params);
     switch (data) {
       case SuccessReturn<Unit>():
-        print("Serviço iniciado com sussesso");
         return unit;
       case ErrorReturn<Unit>():
         throw data.result.message;
